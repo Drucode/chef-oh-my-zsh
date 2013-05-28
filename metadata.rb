0b71346b0020f64d698e931a34b1c5e02a0609df
Mike Conigliaro <mike@conigliaro.org>
2013-05-28 16:51:07 -0600

    Use old git cookbook

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description      'Installs/Configures oh-my-zsh'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends          'git', '~> 2.5.2'
+depends          'git', '~> 0.9.0' # FIXME: Newer versions cause: HTTP Request Returned 500 Internal Server Error: '1.0.' does not match 'x.y.z' or 'x.y'
 depends          'zsh', '~> 1.0.0'
